1097891bdd07f44f7ca77b5bd99bd7e641d352b4
Piotr Wrzodak <piotrwrzodakk@gmail.com>
2023-12-30 21:32:59 +0100

    add Header component

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="fixed top-0 z-10 h-14 bg-white border-b border-gray-200 px-6 flex items-center w-full shadow-sm">
+      <span className="text-lg tracking-wider">DocPolyglot</span>
+    </header>
+  );
+};
